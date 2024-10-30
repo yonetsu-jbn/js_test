031f226b6dfd760b6dedaf7f5925793e04357e0d
yonetsu-jbn <emi_yonetsu@jibun-note.co.jp>
2024-10-30 17:27:45 +0900

    追加２

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,8 +10,67 @@ document.getElementById('table_title').innerHTML = '<h2>従業員一覧</h2>';
 //<td>要素を追加
 let td_1 = '<td>1</td><td>山田太郎</td><td>yamada@exercise.co.jp</td><td>開発</td>';
 let t_body = document.getElementById('table_body');
-t_body.insertAdjacentHTML('afterbegin',td_1);
+// t_body.insertAdjacentHTML('afterbegin',td_1);
 
-//4
+//5
+// console.log('APIリクエストを送信します');
+
+// fetch('http://localhost:3001/employee', { method: 'GET'})
+//     .then(response => response.json())
+//     .then(data =>{
+//         console.log('データを取得しました', data);
+//     })
+//     .catch(error => {
+//         console.error('エラーが発生しました', error);
+//     });
+
+//     console.log('他の処理を実行します')
 
 
+
+    async function getUserData() {
+        try{
+            const response = await fetch('http://localhost:3001/employee');
+            console.log(response)
+            const data= await response.json();
+            console.log(data)
+            const t_body = document.getElementById('table_body');
+
+            for (const userInfo of data) {
+                const userItem = document.createElement('tr');
+                userItem.innerHTML = `
+                    <td>${userInfo.id}</td>
+                    <td>${userInfo.user_name}</td>
+                    <td>${userInfo.mail_address}</td>
+                    <td>${userInfo.dept_name}</td>
+                `;
+                // t_body.insertAdjacentHTML('afterbegin', userItem);
+                t_body.appendChild(userItem);
+
+            }
+            }catch(error){
+                console.error('Error fetching data:', error);
+                t_body.insertAdjacentHTML('afterbegin', `<p>データが取得できませんでした。</p>`);
+            }
+        }
+
+        getUserData();
+
+// 5
+
+// axios.post('http://localhost:3001/employee', { key: 'value' })
+// .then(response => {
+//     console.log('レスポンスデータ:', response.data);
+// })
+// .catch(error => {
+//     console.error('エラーが発生しました:', error);
+// });
+
+document.getElementById('user_name').addEventListener('click', function(){
+
+});
+let user_name = document.getElementById('user_name').value;
+console.log()
+let mail_address = document.getElementById('mail_address');
+let dept_name = document.getElementById('dept_name');
+console.log(user_name);
